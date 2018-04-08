6c09e20bdcded4bec8bdc440225da785386f8472
safiasalha <salhamohdnoh5@gmail.com>
2018-04-08 19:27:37 +0800

    added safia_test.java

---
+++ safia_test.java
@@ -0,0 +1,23 @@
+import java.net.*;
+
+import javav.io.*;
+
+public class Server{
+
+public static void main (String [] args) throws Exception{
+
+ServerSocket s = new ServerSocket(3333);
+
+while(true) {
+
+Socket connectionsocket=s.accept();
+
+BufferedReader messagefromloadbalancer=new BufferedReader(new 
+
+InputStreamReader(connectionsocket.getInputStream());
+
+String sentence = messagefromloadbalancer.readLine();
+
+System.out.println("Receive Connection from the Client:" + sentence);
+
+}
